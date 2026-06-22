bc9e0f2eba3aa87729e7039a984fbaea8c436385
Mochammad Fauzi Nur Fadilah <nurfadilahmfauzi@gmail.com>
2026-06-22 15:10:03 +0700

    push

--- config/cors.go
+++ config/cors.go
@@ -3,6 +3,7 @@ package config
 var allowedOrigins = []string{
 	"http://localhost:5173",
 	"https://my-fe-three.vercel.app",
+	"https://be-latihan-production.up.railway.app",
 }
 
 func GetAllowedOrigins() []string {
--- main.go
+++ main.go
@@ -2,8 +2,10 @@ package main
 
 import (
 	"be_latihan/config"
+	"be_latihan/docs"
 	"be_latihan/model"
 	"be_latihan/router"
+	"os"
 	"strings"
 
 	_ "be_latihan/docs"
@@ -27,6 +29,12 @@ import (
 func main() {
 	app := fiber.New()
 	app.Use(logger.New())
+
+	swaggerHost := os.Getenv("SWAGGER_HOST")
+	if swaggerHost == "" {
+		swaggerHost = "127.0.0.1:3000"
+	}
+	docs.SwaggerInfo.Host = swaggerHost
 
 	app.Use(cors.New(cors.Config{
 		AllowOrigins: strings.Join(config.GetAllowedOrigins(), ","),
